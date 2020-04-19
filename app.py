46ede5b84317d7789a0b6e9a85d7a42583cc4b69
Thomas <thomas.rochette0@gmail.com>
2020-04-19 14:19:29 +0200

    ajout var dans templates

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('index.html')
+    return render_template('index.html', name='Armand')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='8080')
